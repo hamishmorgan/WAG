144656cec3aa93eb94c26be76f3038c65b2c817d
deliprao <deliprao@d3b7648c-96f9-11dd-95d5-4d28051a69de>
2008-10-10 20:00:36 +0000

    Introduced dependency on xerces parser. Need it for the lazy DOM implementation.

--- src/WikiXMLParser.java
+++ src/WikiXMLParser.java
@@ -1,8 +1,25 @@
+/**
+ * A simple API to handle Wikipedia XML dumps
+ * @author delip
+ *
+ */
+
+import org.apache.xerces.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import java.io.IOException;
+
+
+
 
 public class WikiXMLParser {
 	
-
+	String wikiXMLFile = null;
+	boolean useGZip = false;
+	
 	public WikiXMLParser(String fileName) {
+		wikiXMLFile = fileName;
+		if(wikiXMLFile.endsWith(".gz")) useGZip = true;
 	}
 	
 	void process() throws Exception {
@@ -12,6 +29,7 @@ public class WikiXMLParser {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		if(args.length != 1) {
 			System.err.println("Usage: Parser <XML-FILE>");
 			System.exit(-1);
@@ -24,7 +42,5 @@ public class WikiXMLParser {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
-
 	}
-
 }
