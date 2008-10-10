ef0bdc024a10da601bb731e8e3a20590802486d4
deliprao <deliprao@d3b7648c-96f9-11dd-95d5-4d28051a69de>
2008-10-10 20:25:17 +0000

    Added code to lazy DOM the XML file.

--- src/WikiXMLParser.java
+++ src/WikiXMLParser.java
@@ -6,23 +6,39 @@
 
 import org.apache.xerces.parsers.DOMParser;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-import java.io.IOException;
+import org.xml.sax.InputSource;
 
-
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
 
 
 public class WikiXMLParser {
 	
 	String wikiXMLFile = null;
 	boolean useGZip = false;
+	private DOMParser domParser = new DOMParser();
+	private static String FEATURE_URI = 
+		"http://apache.org/xml/features/dom/defer-node-expansion";
 	
-	public WikiXMLParser(String fileName) {
+	public WikiXMLParser(String fileName) throws Exception {
 		wikiXMLFile = fileName;
 		if(wikiXMLFile.endsWith(".gz")) useGZip = true;
+		domParser.setFeature(FEATURE_URI, true);
+		BufferedReader br = null;
+		if(useGZip) {
+			br = new BufferedReader(new InputStreamReader(
+					new GZIPInputStream(new FileInputStream(wikiXMLFile))));
+		} else {
+			br = new BufferedReader(new InputStreamReader( 
+					new FileInputStream(wikiXMLFile)));
+		}
+		domParser.parse(new InputSource(br));
 	}
 	
 	void process() throws Exception {
+		Document doc = domParser.getDocument();
 	}
 
 	/**
@@ -35,9 +51,8 @@ public class WikiXMLParser {
 			System.exit(-1);
 		}
 		
-		WikiXMLParser wxp = new WikiXMLParser(args[0]);
-		
 		try {
+			WikiXMLParser wxp = new WikiXMLParser(args[0]);
 			wxp.process();
 		}catch(Exception e) {
 			e.printStackTrace();
